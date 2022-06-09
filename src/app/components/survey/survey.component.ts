9aa8fd6f9b6adfca7bc3e5770e0c81bbe2408452
Elena Shamshurina <6shamshurina6@gmail.com>
2022-06-09 10:22:29 +0500

    feat(survey): add gis & kii questions

--- src/app/components/survey/survey.component.ts
+++ src/app/components/survey/survey.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { IsSurvey } from '@app/models';
+import { MatCheckboxChange } from '@angular/material/checkbox';
+import { GisSizes } from '@app/constants';
+import { KiiSpecifications } from '@app/constants/kii-specification.const';
+import { IsIspdnSurvey, IsSurvey } from '@app/models';
 
 @Component({
   selector: 'app-survey',
@@ -9,4 +12,24 @@ import { IsSurvey } from '@app/models';
 export class SurveyComponent {
   isFirstStepValid: boolean = false;
   isSurveyResult!: IsSurvey;
+  ispdnSurveyResult!: IsIspdnSurvey;
+
+  gisSizes = GisSizes;
+  kiiSpecifications = KiiSpecifications;
+
+  gisSize!: string;
+  kiiSpecification: string[] = [];
+
+  onCheckboxChange(event: MatCheckboxChange) {
+    if (event.checked) {
+      if (event.source.value !== 'unnessasary') {
+        this.kiiSpecification.push(event.source.value);
+      }
+    } else {
+      const index = this.kiiSpecification.indexOf(event.source.value);
+      if (index !== -1) {
+        this.kiiSpecification.splice(index, 1);
+      }
+    }
+  }
 }
---
+++ src/app/constants/kii-specification.const.ts
@@ -0,0 +1,19 @@
+import { KeyValue } from "@shared/models";
+
+export const KiiSpecifications: KeyValue[] = [
+  {key: 'unnessasary', value: 'Здравоохранение'},
+  {key: 'unnessasary', value: 'Наука'},
+  {key: '', value: 'Транспорт'},
+  {key: 'FINANCIAL', value: 'Банковская сфера'},
+  {key: 'FINANCIAL', value: 'Иные сфера финансового рынка'},
+  {key: 'MANUFACTURE', value: 'Топливно-энергетический комплекс'},
+  {key: 'MANUFACTURE', value: 'Энергетика'},
+  {key: 'MANUFACTURE', value: 'Атомная энергетика'},
+  {key: 'DEFENCE_SYSTEM', value: 'Оборонная промышленность'},
+  {key: 'MANUFACTURE', value: 'Ракетно-космическая промышленность'},
+  {key: 'MANUFACTURE', value: 'Горнодобывающая промышленность'},
+  {key: 'MANUFACTURE', value: 'Металлургическая промышленность'},
+  {key: 'MANUFACTURE', value: 'Химическая промышленность'},
+  {key: '', value: 'Связь'},
+  {key: 'unnessasary', value: 'Здравоохранение'},
+];
